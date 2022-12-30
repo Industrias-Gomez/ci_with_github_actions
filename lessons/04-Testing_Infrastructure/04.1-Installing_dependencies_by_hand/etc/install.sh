cb6dc75c11ad6cd85c05cfe209a0804d0deba902
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-12-30 08:19:11 -0600

    Installing the dependencies of our project 'by hand'

--- lessons/04-Testing_Infrastructure/04.1-Installing_dependencies_by_hand/etc/install.sh
+++ lessons/04-Testing_Infrastructure/04.1-Installing_dependencies_by_hand/etc/install.sh
@@ -1,4 +1,4 @@
 apt-get install mysql
-    apt-get install php74
-    apt-get install rabbitmq
-    pecl install xdebug
+        apt-get install php74
+        apt-get install rabbitmq
+        pecl install xdebug
